f89f17d1c2b4bea589ef7350c47272e9af88079d
Crug09 <Crug09@gmail.com>
2022-04-01 23:16:02 -0400

    main

--- calculator.py
+++ calculator.py
@@ -1,32 +1,38 @@
 from logging import root
 from tkinter import *
-from tkinter import ttk, Text
+from tkinter import ttk, Text, Frame
+from turtle import width
 
+back1 = "#1b1b1b"
+Width = int(176/8)
 
 root = Tk()
 root.title("Calculator")
+root.configure(bg = back1)
 
-width = 500
-height = 700
-root.geometry(f'{width}x{height}')
-root.resizable(False, False)
+W = 540
+H = 700
+root.geometry(f'{W}x{H}')
+# root.resizable(False, False)
 
-def Copy(t):
-    screen1.config(text=text1.get(1.0, 'end-1c'))
-    screen1.update()
+Screen = Label(root,
+    text = "HELLO",
+    width = int(540 / 8))
+Screen.grid(row = 0, column = 0, columnspan = 3)
 
+B1 = Button(root,
+    text = "1",
+    width = Width)
+B1.grid(row = 1, column = 0)
 
-screen1 = Label(root,
-    text = "",)
-screen1.grid(row=0, column=0)
+B2 = Button(root,
+    text = "2",
+    width = Width)
+B2.grid(row = 1, column = 1)
 
-text1 = Text(width = 20, height=1)
-text1.grid(row = 1, column = 0)
-    
-button = Button(root,
-    text = "Copy")
-button.grid(row = 2, column = 0)
-
-button.bind("<Button-1>",Copy)
+B3 = Button(root,
+    text = "3",
+    width = Width)
+B3.grid(row = 1, column = 2)
 
 root.mainloop()
